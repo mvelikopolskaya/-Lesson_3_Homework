e0608876e2f61ffbace15eccb6805f0220bf440f
Cindrom1 <cindrof@gmail.com>
2023-03-03 13:38:29 +0300

    add Task_21

---
+++ Task_19/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Reverse(int num)
+{
+    int index = 0;
+    int numberLenght = int.ToString(number);
+    while(index < numberLenght)
+    {
+
+    }
+
+}
---
+++ Task_21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+
+Console.WriteLine("Введите координаты первой точки: ");
+Console.Write("X1 = ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1 = ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z1 = ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки: ");
+Console.Write("X2 = ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2 = ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z2 = ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double range = Range(X1, Y1, Z1, X2, Y2, Z2);
+Console.WriteLine($"Расстояние между точками в 3D пространстве равно: {Math.Round(range, 2, MidpointRounding.ToZero)}");
+
+
+double Range(int numX1, int numY1, int numZ1, int numX2, int numY2, int numZ2)
+{
+    double result = Math.Sqrt(Math.Pow(numX1 - numX2, 2) + Math.Pow(numY1 - numY2, 2) + Math.Pow(numZ1 - numZ2, 2));
+    return result;
+} 
